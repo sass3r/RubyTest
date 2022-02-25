facad9f856fe0939865a051cbc5f64d651e25f0b
Rafael Ramírez <mrsblast@gmail.com>
2022-02-24 20:05:11 -0400

    Fix pangram

--- pangram.rb
+++ pangram.rb
@@ -1,14 +1,15 @@
 def isPangram?(text)
   pangram = ('a'..'z').all? { |char| text.downcase.include?(char)}
   if pangram
-    puts "Pangram"
+    puts "pangram"
   else
-    puts "Not Pangram"
+    puts "not pangram"
   end
 end
 
 tests = [ 
-		'The quick brown fox jumps over the lazy dog','Pack my box with five dozen liquor jugs',
+		'The quick brown fox jumps over the lazy dog',
+		'Pack my box with five dozen liquor jugs',
 		'Puedo escribir los versos mas tristes esta noche',
 		'Benjamin pidio una bebida de kiwi y fresa Juan sin verguenza la mas exquisita champana del menu',
 		'Luis sabe que una oracion como esta no es un pangrama',
